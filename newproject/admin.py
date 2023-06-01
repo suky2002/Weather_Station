3f2367d2b79f3c968f900bba38f7a7cb1eea896c
Sorana Ion <105973495+suky2002@users.noreply.github.com>
2023-06-01 12:32:17 +0100

    Delete newproject directory

--- newproject/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import WeatherData
-
-class WeatherDataAdmin(admin.ModelAdmin):
-    list_display = ['temperature', 'humidity', 'pressure', 'altitude','created_at']
-
-admin.site.register(WeatherData, WeatherDataAdmin)
